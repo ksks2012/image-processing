4fc9039e3e69e84d4864c37dea3f84eb5ff997c1
ksks2012 <st890751999@gmail.com>
2022-06-15 08:15:21 +0800

    docs(test_shell): add shell for compiling all test

---
+++ test_shell.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+test_list=(test_ppm_rw test_transforms test_pgm_rw)
+
+for test_target in ${test_list[@]} 
+do
+    echo ${test_target}
+    make all target=${test_target}
+done
+
